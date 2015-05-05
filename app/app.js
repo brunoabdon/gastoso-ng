ba38e6fb5b858c20b36e04264f5caece9e137248
Bruno Abdon <bru.n.oa.b.don@gmail.com>
2015-05-05 01:27:15 -0300

    ainda...

--- app/app.js
+++ app/app.js
@@ -1,12 +1,32 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('gastosoApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'ngResource',
+  'gastosoServices',
+  'gastosoApp.contas'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/contas'});
 }]);
+
+
+var gastosoService = angular.module('gastosoServices', ['ngResource']);
+
+gastosoService.factory('Conta', ['$resource',
+  function($resource){
+//      return $resource('http://localhost:5000/contas/');
+
+    return $resource('http://localhost:5000/contas/:contaId', {}, {
+      query: {method:'GET', params: {contaId:'@id'} ,isArray:true}
+    });
+
+/*
+  var res = new Object();
+  res.query = function(){return [{"id":1,"nome":"Carteira"},{"id":2,"nome":"Santander"},{"id":3,"nome":"Banco do Brasil"},{"id":4,"nome":"Cartão de Crédito"}];};
+
+return res
+*/
+
+  }]);
