2aa34d28464dd4fac5aedd4147e4d00b4e970130
brunoabdon <bru.n.oa.b.don@gmail.com>
2015-12-02 16:03:53 -0200

    modernizando deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
-git subtree push --prefix app github gh-pages
+DISTDIR=".dist"
+
+if [ ! -d "$DISTDIR" ]; then
+  git clone https://github.com/brunoabdon/gastoso.git -b gh-pages --depth 1 --separate-git-dir .deploygit  $DISTDIR
+fi
+
+cd $DISTDIR
+git checkout .
+cd ..
+rm -r $DISTDIR/*
+cp -r app/* $DISTDIR
+cp bower.json $DISTDIR
+cd $DISTDIR
+echo "{\"directory\": \"bower_components\"}" > .bowerrc
+bower install
+echo ".bowerrc\nbower.json\n.gitignore" > .gitignore
+git add --all .
+git status
+git commit -m 'publicando'
+git push 
 
